70c2f3238c3e03b9f1dad28291214e748b71b5a4
Mojtaba Ahadi <mojtaba_2424@yahoo.com>
2019-01-13 16:18:16 +0330

    version added

---
+++ __version__.py
@@ -0,0 +1,7 @@
+__title__ = 'yahoo_weather'
+__description__ = 'Python library for yahoo weather new API'
+__url__ = 'https://github.com/M-Ahadi/yahoo_weather'
+__version__ = '1.0.0'
+__author__ = 'Mojtaba Ahadi'
+__author_email__ = 'm.ahadi@outlook.com'
+__license__ = 'Apache 2.0'
