b35d144ddc30b7ca4630a56fc19308e998e3c22b
Mojtaba <mojtaba_2424@yahoo.com>
2019-02-11 12:14:34 +0330

    just cleaning code

--- yahoo_weather/config/config.py
+++ yahoo_weather/config/config.py
@@ -1,4 +1,3 @@
-
 class yahoo:
     url_city = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location={city}&format=json&u={unit}"
     url_location = "https://weather-ydn-yql.media.yahoo.com/forecastrss?lat={lat}&lon={lon}&format=json&u={unit}"
