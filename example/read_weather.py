8243f8c8d52e37fea18f44335e66b3caf44b2597
Mojtaba <mojtaba_2424@yahoo.com>
2019-04-04 12:07:26 +0430

    fix: example added to the source

--- example/read_weather.py
+++ example/read_weather.py
@@ -1,16 +1,10 @@
-# Python Library for Yahoo Weather (New API)
-On 3 Jan 2019 Yahoo changed its Weather API to use OAuth1 Authentication. This new method requires authentication to use the API. This library helps you to handle the new API.
-In order to use the new API you have to get an API key from [Yahoo!](https://developer.yahoo.com/weather/?guccounter=1)
-
-
-```
 from yahoo_weather.weather import YahooWeather
 from yahoo_weather.config.units import Unit
 
-data = yahoo_weather.YahooWeather(APP_ID="Your App ID",
+weather = YahooWeather(APP_ID="Your App ID",
                      api_key="Your API KEY",
                      api_secret="Your API secret")
-                     
+
 weather.get_yahoo_weather_by_city("tehran", Unit.celsius)
 print(weather.condition.text)
 print(weather.condition.temperature)
@@ -18,4 +12,3 @@ print(weather.condition.temperature)
 weather.get_yahoo_weather_by_location(35.67194, 51.424438)
 print(weather.condition.text)
 print(weather.condition.temperature)
-```
